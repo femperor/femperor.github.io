d2e1a44756721ad5ffbc3629551c0c3831baa72a
zhiou <zhiou@MacBook-Pro.local>
2020-10-27 23:07:54 +0800

    - fix name & email

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -43,8 +43,8 @@ flush() {
 }
 
 deploy() {
-  git config --global user.name "GitHub Actions"
-  git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+  git config --global user.name "femperor"
+  git config --global user.email "femperor@gmail.com"
 
   git update-ref -d HEAD
   git add -A
